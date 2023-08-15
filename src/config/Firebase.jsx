1696ef023291c93539014daa0bd140c0ff8820c6
Abhishek k <bobbyyyyy.16@gmail.com>
2023-08-15 12:06:45 +0530

    Firebase configuration

---
+++ src/config/Firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkc-htF-UWeKY1vzgqFVags9mTNVR478s",
+  authDomain: "fir-course-26329.firebaseapp.com",
+  projectId: "fir-course-26329",
+  storageBucket: "fir-course-26329.appspot.com",
+  messagingSenderId: "984718911683",
+  appId: "1:984718911683:web:2166f91063b9cc2cf8da76",
+  measurementId: "G-LHZBRKFTP8"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
